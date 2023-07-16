1a314e8903c0f851aeb3ff4315803bb111a53349
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-07-16 23:01:07 +0500

    deployed backend on server

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const mongoose = require('mongoose');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 mongoose
-  .connect('mongodb://0.0.0.0:27017/test', {
+  .connect("mongodb://0.0.0.0:27017/test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('Database Connected'))
+  .then(() => console.log("Database Connected"))
   .catch((err) => {
     console.log(err);
   });
 
-const express = require('express');
+const express = require("express");
+
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 
-const userRoute = require('./routes/userRoute');
+const userRoute = require("./routes/userRoute");
 
-app.use('/', userRoute);
+app.use(express.static(path.join(__dirname + "public")));
 
-app.listen(6010, () => {
-  console.log('app is running');
+app.use("/", userRoute);
+
+const PORT = process.env.PORT || 6010;
+
+app.listen(PORT, () => {
+  console.log("app is running");
 });
