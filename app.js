908e185b0a68accc9c89e01bf79c3a18a814f0b4
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-07-19 20:25:59 +0500

    added start command

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname + "/public")));
 
 app.use("/", userRoute);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6010;
 
 app.listen(PORT, () => {
   console.log("app is running");
