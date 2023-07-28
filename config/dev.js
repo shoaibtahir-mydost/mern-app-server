5234a0b5ae8c0e465536cdf2301f2ac7e306d6ad
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-07-28 17:16:15 +0500

    updated the configuration files for dev and prod

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+module.export = {
+  GOOGLE_CLIENT_ID:
+    "305873482596-r4tkb9bh5cj6bq5c3kk4k1skklgk40e4.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "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",
+};
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.export = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+};
