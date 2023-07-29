6e748e0e1d553f947057d9c8bf3193b5a21ebda0
Muhammad Shoaib Tahir <shoaibtahir97@gmail.com>
2023-07-29 12:24:17 +0500

    added mongodb database string in dev.js file

--- config/dev.js
+++ config/dev.js
@@ -2,4 +2,6 @@ module.export = {
   GOOGLE_CLIENT_ID:
     "305873482596-r4tkb9bh5cj6bq5c3kk4k1skklgk40e4.apps.googleusercontent.com",
   GOOGLE_CLIENT_SECRET: "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",
+  MONGODB_CONNECTION_STRING:
+    "mongodb+srv://shoaibtahir97:BsBmNfEo0CqlUeK5@patentcluster0.7qpftw6.mongodb.net/?retryWrites=true&w=majority",
 };
